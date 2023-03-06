4881b73a6a710cb6fb61d1df9d88f0dcbdeaa449
Thatcher Auten <thatcherauten24@gmail.com>
2023-03-06 11:20:57 -0600

    Point2D Complete

--- Point2D.java
+++ Point2D.java
@@ -1,3 +1,52 @@
 public class Point2D {
+    private double x;
+    private double y;
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double newX) {
+        x = newX;
+    }
     
+    public void setY(double newY) {
+        y = newY;
+    }
+
+    public double distanceTo(Point2D other) {
+        double distance = 0.0;
+
+        distance = Math.sqrt((this.x - other.x) * (this.x - other.x) + (this.y - other.y) * (this.y - other.y));
+
+        return distance;
+    }
+
+    public String toString() {
+        String result = "";
+        result += "(";
+        result += x;
+        result += ", ";
+        result += y;
+        result += ")";
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Point2D p1 = new Point2D(0, 2);
+        Point2D p2 = new Point2D(0, 0);
+
+        System.out.println(p1);
+        System.out.println(p1.distanceTo(p2));
+    }
 }
